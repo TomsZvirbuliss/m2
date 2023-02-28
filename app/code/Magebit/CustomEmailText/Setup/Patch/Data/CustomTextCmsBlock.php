c4c1120ba5b4147f59c53067bfe400173e23fd48
TomsZvirbuliss <toms.zvirbulis@magebit.com>
2023-02-28 10:38:28 +0200

    Transaction email custom text

---
+++ app/code/Magebit/CustomEmailText/Setup/Patch/Data/CustomTextCmsBlock.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * This file is part of the Magebit package.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magebit Faq
+ * to newer versions in the future.
+ *
+ * @copyright Copyright (c) 2022 Magebit, Ltd. (https://magebit.com/)
+ * @license   GNU General Public License ("GPL") v3.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Magebit\CustomEmailText\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Cms\Model\BlockFactory;
+
+/**
+ * Class AddNewCmsStaticBlock
+ * @package Rbj\CmsBlock\Setup\Patch\Data
+ */
+class CustomTextCmsBlock implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var BlockFactory
+     */
+    private $blockFactory;
+
+    /**
+     * AddAccessViolationPageAndAssignB2CCustomers constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param PageFactory $blockFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        BlockFactory             $blockFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->blockFactory = $blockFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $cmsData = [
+            'title' => 'Add Email Custom Text',
+            'identifier' => 'email_cms_block',
+            'content' => '',
+            'is_active' => 1,
+            'stores' => \Magento\Store\Model\Store::DEFAULT_STORE_ID
+        ];
+
+        $this->blockFactory->create()->setData($cmsData)->save();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getVersion()
+    {
+        return '2.0.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
